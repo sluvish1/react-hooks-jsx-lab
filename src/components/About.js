9821e0e9e63493234639d0ae8771f68b985bb171
Vaschell Mkenzie <mvaschell@gmail.com>
2023-05-21 18:45:20 -0400

    jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="About">
+      <h2>"About Me"</h2>
+      <p>"This is cool"</p>
+      <img
+        src={image} alt="I made this">
+      </img>
+    </div>
+  )
 }
 
 export default About;
