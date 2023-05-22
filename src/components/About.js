1bb87c1928b3f37638a0bdc9c6f1bd0c01eb08f3
Vaschell Mkenzie <mvaschell@gmail.com>
2023-05-22 18:21:25 -0400

    corrected

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
+const About = () => {
   return (
-    <div id="About">
-      <h2>"About Me"</h2>
-      <p>"This is cool"</p>
-      <img
-        src={image} alt="I made this">
-      </img>
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is cool</p>
+      <img src={image} alt="I made this"></img>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ function Home() {
   // update the JSX being returned!
   return (
   <div id="home">
-    <h1 style={{color:"firebrick"}}>
-    "Your Name is {name} from {city}"
+    <h1 style={ {color:"firebrick"} }>
+        {name} is a Web Developer from {city}
     </h1>
   </div>
   )
